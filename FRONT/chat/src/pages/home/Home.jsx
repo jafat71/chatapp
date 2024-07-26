e2cd5b10f6d08af2edfe27e9bbcc01b20724bfc8
jzafvt771 <ijmm7171@gmail.com>
2024-07-26 04:20:35 -0500

    refix keyboard

--- FRONT/chat/src/pages/home/Home.jsx
+++ FRONT/chat/src/pages/home/Home.jsx
@@ -1,49 +1,47 @@
-import { useEffect, useRef } from "react";
-import Panel from "../../components/panel/Panel";
-import Sidebar from "../../components/sidebar/Sidebar";
-import { useDisplayDispatch } from "./context/DisplayModeContext";
+import { useEffect } from "react";
+import Panel from "../../components/panel/Panel"
+import Sidebar from "../../components/sidebar/Sidebar"
+import {useDisplayDispatch } from "./context/DisplayModeContext";
 import useContact from "../../zustand/useContact";
 
 const Home = () => {
-  const dispatchDisplay = useDisplayDispatch();
-  const { setSelectedContact } = useContact();
-  const initialWindowHeight = useRef(window.innerHeight);
 
+  const dispatchDisplay = useDisplayDispatch()
+  const {setSelectedContact} = useContact()
   useEffect(() => {
     const handleResize = () => {
       const isMd = window.matchMedia("(min-width: 768px)").matches;
-      const currentWindowHeight = window.innerHeight;
-      const isKeyboardOpen = initialWindowHeight.current > currentWindowHeight;
-
-      if (!isKeyboardOpen) {
-        if (isMd) {
-          dispatchDisplay({ type: "BOTH" });
-        } else {
-          dispatchDisplay({ type: "CONTACT" });
-        }
+      
+      if (isMd) {
+        dispatchDisplay({ type: "BOTH" })
+      } else {
+        dispatchDisplay({ type: "CONTACT" })
       }
     };
 
-    handleResize();
+    handleResize()
     window.addEventListener("resize", handleResize);
-
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  }, [dispatchDisplay]);
+  }, []);
 
   useEffect(() => {
     return () => {
-      setSelectedContact(null);
+      setSelectedContact(null)
     };
-  }, [setSelectedContact]);
+  }, []);
+
 
   return (
-    <div className="flex flex-col-reverse md:flex-row justify-center items-center bg-base-100 rounded-lg text-white w-3/4">
-      <Sidebar />
-      <Panel />
+    <>
+      <div className="flex flex-col-reverse md:flex-row justify-center items-center bg-base-100 rounded-lg text-white w-full md:w-3/4 ">
+        <Sidebar></Sidebar>
+        <Panel></Panel>
     </div>
-  );
-};
+    </>
 
-export default Home;
+  )
+}
+
+export default Home
