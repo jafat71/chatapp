d991e2d58056088ed772784c31699dfdb3006fa7
jzafvt771 <ijmm7171@gmail.com>
2024-07-27 06:25:22 -0500

    fix: update message notif

--- FRONT/chat/src/components/sidebar/Contact.jsx
+++ FRONT/chat/src/components/sidebar/Contact.jsx
@@ -1,26 +1,32 @@
 /* eslint-disable react/prop-types */
 import { useConnectedUsersValue } from "../../context/SocketContext";
 import { useDisplayDispatch } from "../../pages/home/context/DisplayModeContext";
-import { useLiveMessageValue } from "../../pages/home/context/LiveContext";
+import { useLiveMessageDispatch, useLiveMessageValue } from "../../pages/home/context/LiveContext";
 import useContact from "../../zustand/useContact";
 import ChatIcon from "./ChatIcon"
 
 const Contact = ({conversation}) => {
+    const liveMessages = useLiveMessageValue()
+    const disptachLiveMessages = useLiveMessageDispatch()
     const dispatchDisplay = useDisplayDispatch()
     const handleClick = () => {
         (!window.matchMedia("(min-width: 768px)").matches) && dispatchDisplay({ type: "CHAT" })
         setSelectedContact(conversation)
+        disptachLiveMessages({type:"UNSET"})
     };
     const {selectedContact,setSelectedContact } = useContact()
 
     const isSelected = selectedContact?._id === conversation._id
     const onlineUsers =  useConnectedUsersValue();
     const isOnline = onlineUsers.includes(conversation._id)
-    const liveMessages = useLiveMessageValue()
-    console.log("LIVE")
-    console.log(liveMessages)
+    const hasNewMessages = liveMessages.senderId===conversation._id
+    console.log(liveMessages.senderId)
+    console.log(hasNewMessages)
     return (
-        <div className={`flex flex-row items-center justify-between hover:bg-gray-200  hover:text-black transition-all duration-200 rounded p-2 py-1 cursor-pointer w-full ${isSelected ?" bg-rose-600 ":""}`} onClick={handleClick}>
+        <div className={`flex flex-row items-center justify-between hover:bg-gray-200  hover:text-black transition-all duration-200 rounded p-2 py-1 cursor-pointer w-full 
+        ${isSelected ?" bg-rose-600 ":""}
+        ${hasNewMessages ? "bg-green-500" : ""}
+        `} onClick={handleClick}>
             <div className="p-2 rounded-sm flex flex-row items-center gap-1 text-sm">
                 <div className={`avatar ${isOnline ?' online':'offline'}`}>
                     <div className="w-8 rounded-full">
